14cc9f20a94cbeb7335afc3330fc69c4f8b4c775
Mikhail Nikolenko <kompreso@vk.com>
2024-07-04 09:36:07 +0500

    Remove unused 'use'

--- app/Http/Requests/EquipmentStoreRequest.php
+++ app/Http/Requests/EquipmentStoreRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
